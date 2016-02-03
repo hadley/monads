c456d53c3fb651811f66b1f485c7ee42a564d823
hadley <h.wickham@gmail.com>
2016-02-03 14:43:49 -0600

    Let %>>% accept more types of input

--- R/infix.R
+++ R/infix.R
@@ -13,11 +13,38 @@
 #' r <- reactive(iris)
 #' r2 <- r %>>% head(5)
 #' r2()
+#'
+#' double <- function(x) x * 2
+#' maybe(10) %>>% double
+#' maybe(10) %>>% double()
+#' maybe(10) %>>% function(x) x * 2
 #' }
 "%>>%" <- function(lhs, rhs) {
   lhs <- substitute(lhs)
   rhs <- substitute(rhs)
 
-  call_fmap <- as.call(c(quote(fmap), lhs, rhs[[1]], as.list(rhs[-1])))
+  if (singular_form(rhs)) {
+    call_fmap <- as.call(c(quote(fmap), lhs, rhs))
+  } else {
+    call_fmap <- as.call(c(quote(fmap), lhs, rhs[[1]], as.list(rhs[-1])))
+  }
+
   eval(call_fmap, parent.frame())
+}
+
+singular_form <- function(x) {
+  if (is.name(x))
+    return(TRUE)
+
+  x <- x[[1]]
+  if (identical(x, quote(`function`)))
+    return(TRUE)
+
+  if (identical(x, quote(`(`)))
+    return(TRUE)
+
+  if (identical(x, quote(`{`)))
+    return(TRUE)
+
+  FALSE
 }
